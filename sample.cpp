590e0faeac4332b8fcace3c191dbe094448fb514
ORosado-0 <143463853+ORosado-0@users.noreply.github.com>
2025-08-26 17:50:00 +0000

    completed assignment

--- sample.cpp
+++ sample.cpp
@@ -1,7 +1,18 @@
-// YOUR NAME HERE
+// Osvaldo Rosado
 #include <iostream>
 using namespace std;
 
 int main () {
+int a,b, sum;
 
+cout << "Input first integer: ";
+    cin >> a;
+cout << "Input second integer: ";
+    cin >> b;
+
+sum = a + b;
+
+cout << "Sum of inegers is: " << sum << endl;
+
+return 0;
 }
